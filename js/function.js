6259273e623e529f40d647ecaa696782f31b5788
ninedril@4230s <papiyou.oguper.4230s@gmail.com>
2017-11-19 11:51:21 +0900

    function.js created

---
+++ js/function.js
@@ -0,0 +1,9 @@
+function VideoManager() {
+	this.video = document.getElementsByTagName('video')[0]
+	this.play = function() { 
+		this.video.paused ? this.video.play() : this.video.pause();
+    }
+    this.timer = function(wait_min) {
+        setTimeout(this.play.bind(this), wait_min*60*1000);
+    }
+}
